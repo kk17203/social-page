65152d7c39fe605676c637199d7b89cf36a6d88f
kk17203 <kk17203@gmail.com>
2024-01-05 07:02:21 -0600

    Created a users page with a list of all users signed up.

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const asyncHandler = require("express-async-handler");
+const User = require("../models/users");
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-    res.send("users page");
-});
+router.get(
+    "/",
+    asyncHandler(async (req, res, next) => {
+        // Make sure user is logged in
+        if (!req.user) {
+            return res.redirect("/");
+        }
+
+        const users = await User.find({}).exec();
+
+        res.render("users", { title: "Users Page", users: users });
+    })
+);
 
 module.exports = router;
