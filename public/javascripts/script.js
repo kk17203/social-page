a8e78f233276db5dba5c5be5005b1b703df358a0
kk17203 <kk17203@gmail.com>
2024-01-09 07:02:57 -0600

    Add style to comments form. Note: commentsForm display is block for dev.

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -5,15 +5,50 @@ function submitLikeForm(index) {
 }
 
 function openCommentsForm(index) {
-    const commentsForm = document.getElementById(`comments-form${index}`);
-
+    // Find commentsForm div using passed index
+    const commentsForm = document.getElementById(
+        `commentsFormContainer${index}`
+    );
     const currentDisplay = commentsForm.style.display;
+    // Toggle commentsForm display
     commentsForm.style.display = currentDisplay === "none" ? "block" : "none";
 
+    // Find outlined comment icon using passed index
+    const iconReg = document.getElementById(`iconReg${index}`);
+    const iconRegDisplay = iconReg.style.display;
+    // Toggle outlined comment icon display
+    iconReg.style.display = iconRegDisplay === "none" ? "block" : "none";
+
+    // Find solid comment icon using passed index
+    const iconSolid = document.getElementById(`iconSolid${index}`);
+    let iconSolidDisplay = iconSolid.style.display;
+    // Toggle solid comment icon display
+    iconSolid.style.display = iconSolidDisplay === "none" ? "block" : "none";
+
+    // If comments form is open (remember this is after we JUST opened it in above code)
     if (commentsForm.style.display === "block") {
+        // Find text area and focus on it
         const textarea = document.getElementById(`commentContent${index}`);
         if (textarea) {
             textarea.focus();
         }
+        // Add window event listener to hide comments form when clicked outside
+        window.addEventListener("click", function (event) {
+            //Check to make sure click isn't inside the commentsForm or the regular icon
+            const isClickInsideForm =
+                commentsForm.contains(event.target) ||
+                event.target === commentsForm;
+            const isClickOnIconReg = event.target === iconReg;
+
+            // If click is anywhere but these 2 places...
+            if (!isClickInsideForm && !isClickOnIconReg) {
+                // Close comments form
+                commentsForm.style.display = "none";
+                // Hide solid comment icon
+                iconSolid.style.display = "none";
+                // Show outlined comment icon
+                iconReg.style.display = "block";
+            }
+        });
     }
 }
