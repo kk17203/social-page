25e29d990db7467567913b0664d1b95dee7468c1
kk17203 <kk17203@gmail.com>
2024-01-08 19:26:21 -0600

    Added ability to open and close comment form.

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,3 +3,11 @@ function submitLikeForm(index) {
     // Pass through index of post so we 'like' the correct post
     document.getElementById(`likeForm${index}`).submit();
 }
+
+function openCommentsForm(index) {
+    const commentsForm = document.getElementById(`comments-form${index}`);
+    commentsForm.style.display =
+        commentsForm.style.display === "none" ? "block" : "none";
+}
+
+// PROBLEM IS HERE
