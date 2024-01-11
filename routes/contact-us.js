f6f13d40a8f551cdb6bdbe164c29d885a5945b2a
kk17203 <kk17203@gmail.com>
2024-01-10 20:55:27 -0600

    Added style to contact page.

--- routes/contact-us.js
+++ routes/contact-us.js
@@ -20,13 +20,14 @@ router.get(
     "/",
     asyncHandler(async (req, res, next) => {
         // Make sure user is logged in
-        // if (!req.user) {
-        //     return res.redirect("/");
-        // }
+        if (!req.user) {
+            return res.redirect("/");
+        }
 
         res.render("contactUs", {
             title: "Contact Us",
             user: req.user,
+            currentPage: "/contact",
         });
     })
 );
@@ -35,7 +36,21 @@ router.get(
 router.post(
     "/",
     asyncHandler(async (req, res, next) => {
-        const { name, email, message } = req.body;
+        const { name, email, message, userRealName, userName, userId } =
+            req.body;
+
+        // Validate required fields
+        if (!name || !message) {
+            console.log(
+                "Tried to send blank email. ---------------------------------------------------------------------------"
+            );
+            return res.render("contactUs", {
+                title: "Contact Us",
+                user: req.user,
+                currentPage: "/contact",
+                errorMessage: "Please provide name, email, and message.",
+            });
+        }
 
         const mailOptions = {
             from: {
@@ -45,24 +60,38 @@ router.post(
             to: process.env.MY_EMAIL,
             subject: "New Contact Form Submission",
             text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
-            html: `<p><strong>Name:</strong> ${name}</p>
+            html: `<h3>Form Input</h3>
+                   <p><strong>Name:</strong> ${name}</p>
                    <p><strong>Email:</strong> ${email}</p>
-                   <p><strong>Message:</strong> ${message}</p>`,
+                   <p><strong>Message:</strong> ${message}</p>
+                   <h3>User Info</h3>
+                   <p><strong>Users Name:</strong> ${userRealName}</p>
+                   <p><strong>Username:</strong> ${userName}</p>
+                   <p><strong>UserId:</strong> ${userId}</p>`,
         };
 
         try {
             await transporter.sendMail(mailOptions);
-            console.log("Email has been sent!");
-            res.render("contactUs", {
-                title: "Contact Us",
-                user: req.user,
-                successMessage: "Email sent successfully!",
-            });
+            console.log(
+                "Email has been sent!--------------------------------------------------------------------------------"
+            );
+            // Make sure user is logged in
+            if (!req.user) {
+                return res.redirect("/");
+            }
+
+            res.redirect("/");
         } catch (error) {
             console.error(error);
+            // Make sure user is logged in
+            if (!req.user) {
+                return res.redirect("/");
+            }
+
             res.render("contactUs", {
                 title: "Contact Us",
                 user: req.user,
+                currentPage: "/contact",
                 errorMessage: "Error sending email.",
             });
         }
