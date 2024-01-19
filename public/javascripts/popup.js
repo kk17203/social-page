ed21180a7ca138a8386e8d847701dc612a519d03
kk17203 <kk17203@gmail.com>
2024-01-18 20:08:54 -0600

    Added style to profile pic selection popup.

--- public/javascripts/popup.js
+++ public/javascripts/popup.js
@@ -2,48 +2,11 @@ function selectProfilePicture(labelId, imageId) {
     const element = document.getElementById(labelId);
     element.selected = true;
     const image = document.getElementById(imageId);
-    image.classList.add("selected-profile-pic");
+    image.classList.add("selected-profile-pic-signup");
     window.addEventListener("click", function (event) {
         if (event.target !== element && event.target !== image) {
             element.selected = false;
-            image.classList.remove("selected-profile-pic");
+            image.classList.remove("selected-profile-pic-signup");
         }
     });
 }
-
-/// END JS for Select Profile Pic form ///
-
-// // User page submit form
-// function submitUserPageForm() {
-//     document.getElementById("userPageForm").submit();
-// }
-
-// /// FOLLOWERS POP-UP ///
-
-// document.getElementById("followers").addEventListener("click", function () {
-//     document.getElementById("followersPopUp").style.display = "block";
-// });
-
-// // Close the modal if user clicks outside of it
-// window.addEventListener("click", function (event) {
-//     const followersPopUp = document.getElementById("followersPopUp");
-
-//     if (event.target === followersPopUp) {
-//         followersPopUp.style.display = "none";
-//     }
-// });
-
-// /// FOLLOWING POP-UP ///
-
-// document.getElementById("following").addEventListener("click", function () {
-//     document.getElementById("followingPopUp").style.display = "block";
-// });
-
-// // Close the modal if user clicks outside of it
-// window.addEventListener("click", function (event) {
-//     const followingPopUp = document.getElementById("followingPopUp");
-
-//     if (event.target === followingPopUp) {
-//         followingPopUp.style.display = "none";
-//     }
-// });
