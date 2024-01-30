d895c2bf5890974e195a98f2bbf31146c78b7757
kk17203 <kk17203@gmail.com>
2024-01-30 06:11:10 -0600

    Removed new user email to both user and admin.

--- routes/features.js
+++ routes/features.js
@@ -56,57 +56,57 @@ router.get(
         //     }
         // } else {
 
-        if (req.user.username !== "employer") {
-            /// EMAIL ADMIN IF A NEW USER LOGS IN ///
-            const mailOptions = {
-                from: {
-                    name: "NEW USER LOGIN",
-                    address: process.env.USER_EMAIL,
-                },
-                to: process.env.MY_EMAIL,
-                subject: "New User Login",
-                text: `Login Info: ${req.user.formattedLastLogin}`,
-                html: `<h3>Login Info</h3>
-                <p>User: ${req.user.name}</p>
-                <p>Username: ${req.user.username}</p>
-                   <p>Time: ${req.user.formattedLastLogin}</p>
-                   <p>Ip Address: ${req.user.formattedLastLoginIp}`,
-            };
+        // if (req.user.username !== "employer") {
+        /// EMAIL ADMIN IF A NEW USER LOGS IN ///
+        //     const mailOptions = {
+        //         from: {
+        //             name: "NEW USER LOGIN",
+        //             address: process.env.USER_EMAIL,
+        //         },
+        //         to: process.env.MY_EMAIL,
+        //         subject: "New User Login",
+        //         text: `Login Info: ${req.user.formattedLastLogin}`,
+        //         html: `<h3>Login Info</h3>
+        //         <p>User: ${req.user.name}</p>
+        //         <p>Username: ${req.user.username}</p>
+        //            <p>Time: ${req.user.formattedLastLogin}</p>
+        //            <p>Ip Address: ${req.user.formattedLastLoginIp}`,
+        //     };
 
-            try {
-                await transporter.sendMail(mailOptions);
-                console.log(
-                    "User Email has been sent!--------------------------------------------------------------------------------"
-                );
-            } catch (error) {
-                console.error(error);
-            }
+        //     try {
+        //         await transporter.sendMail(mailOptions);
+        //         console.log(
+        //             "User Email has been sent!--------------------------------------------------------------------------------"
+        //         );
+        //     } catch (error) {
+        //         console.error(error);
+        //     }
 
-            /// EMAIL NEW USER THEIR WELCOME EMAIL ON FIRST LOGIN ///
-            const userMailOptions = {
-                from: {
-                    name: "Welcome!",
-                    address: process.env.USER_EMAIL,
-                },
-                to: req.user.email,
-                subject: "Welcome to Polk County Cycling!",
-                text: `Welcome, ${req.user.name}! We want to take a moment to thank you for visiting Polk County Cycling! If you have any questions or feekback don't hesitate to contact us via our website or our email, pococycling@gmail.com.`,
-                html: `<h3>Welcome, ${req.user.name}!</h3>
-                <p>We want to take a moment to thank you for visiting Polk County Cycling! If you have any questions or feekback don't hesitate to contact us via our website or our email, pococycling@gmail.com.</p>
-                <p>Have a GREAT DAY and don't forget to enjoy the ride!</p>
-                <p>Sincerely,</p>
-                <p>Polk County Cycling Club</p>`,
-            };
+        //     /// EMAIL NEW USER THEIR WELCOME EMAIL ON FIRST LOGIN ///
+        //     const userMailOptions = {
+        //         from: {
+        //             name: "Welcome!",
+        //             address: process.env.USER_EMAIL,
+        //         },
+        //         to: req.user.email,
+        //         subject: "Welcome to Polk County Cycling!",
+        //         text: `Welcome, ${req.user.name}! We want to take a moment to thank you for visiting Polk County Cycling! If you have any questions or feekback don't hesitate to contact us via our website or our email, pococycling@gmail.com.`,
+        //         html: `<h3>Welcome, ${req.user.name}!</h3>
+        //         <p>We want to take a moment to thank you for visiting Polk County Cycling! If you have any questions or feekback don't hesitate to contact us via our website or our email, pococycling@gmail.com.</p>
+        //         <p>Have a GREAT DAY and don't forget to enjoy the ride!</p>
+        //         <p>Sincerely,</p>
+        //         <p>Polk County Cycling Club</p>`,
+        //     };
 
-            try {
-                await transporter.sendMail(userMailOptions);
-                console.log(
-                    "New User Email has been sent!--------------------------------------------------------------------------------"
-                );
-            } catch (error) {
-                console.error(error);
-            }
-        }
+        //     try {
+        //         await transporter.sendMail(userMailOptions);
+        //         console.log(
+        //             "New User Email has been sent!--------------------------------------------------------------------------------"
+        //         );
+        //     } catch (error) {
+        //         console.error(error);
+        //     }
+        // }
         // }
 
         res.render("features", {
